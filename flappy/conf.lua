987b3aa7a95640b0caae07cd67c34d824ed2504d
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-01-25 21:54:09 +0900

    fixed parallax - for now

---
+++ flappy/conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+    t.window.height = 288;
+    t.window.width = 512;
+end
--- flappy/main.lua
+++ flappy/main.lua
@@ -1,26 +1,36 @@
 require "player"
 
-GRAVITY = 1000
+GRAVITY = 500
+
+local BACKGROUND_SCROLL_SPEED = 300
+local BACKGROUND_LOOPING_POINT = 413
 
 
 function love.load()
 
-    background = love.graphics.newImage("graphics/Sky.png")
+    background = love.graphics.newImage("graphics/background.png")
     ground = love.graphics.newImage("graphics/ground.png")
+
+    backgroundScroll = 0
 
     player_load()
 
 end
 
 function love.update(dt)
+
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+
     player_update(dt)
 end
 
 function love.draw()
 
     --background
-    love.graphics.draw(background, 0, 0)
-    love.graphics.draw(ground, 0, 280)
+    love.graphics.draw(background, -backgroundScroll, 0)
+    love.graphics.draw(ground, 0, 272)
+    love.graphics.print(backgroundScroll, 0, 400)
+    
 
     player_draw()
 end
